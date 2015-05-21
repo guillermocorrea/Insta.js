07b0db6b53d10368a8bdb17f8909be7a878ceb09
guillermocorrea <luis.guillermo.correa@hotmail.com>
2015-05-21 01:13:02 -0500

    Login page

--- client/app.js
+++ client/app.js
@@ -1,8 +1,23 @@
 /**
  * Created by LuisGuillermo on 5/6/2015.
  */
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
-    .config(function($routeProvider) {
+/**
+ * The configuration module.
+ */
+angular.module('Instagram.config', [])
+    .constant('CONFIG_URL', {
+        BACKEND_BASE_URI: 'http://localhost:3000/',
+        REDIRECT_URI: 'http://localhost:8080/',
+        CLIENT_ID: '03d3fb648d39408da13776b1d7b8421a',
+        IG_AUTHORIZATION_ENDPOINT: 'https://api.instagram.com/oauth/authorize'
+    });
+
+
+/**
+ * The main module
+ */
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer', 'Instagram.config'])
+    .config(function($routeProvider, $authProvider, CONFIG_URL) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/home.html',
@@ -21,4 +36,17 @@ angular.module('Instagram', ['ngRoute', 'ngMessages'])
                 controller: 'DetailCtrl'
             })
             .otherwise('/');
+
+        $authProvider.loginUrl = CONFIG_URL.BACKEND_BASE_URI + 'auth/login';
+        $authProvider.signupUrl = CONFIG_URL.BACKEND_BASE_URI + 'auth/signup';
+        $authProvider.oauth2({
+            name: 'instagram',
+            url: CONFIG_URL.BACKEND_BASE_URI + 'auth/instagram',
+            redirectUri: CONFIG_URL.REDIRECT_URI,
+            clientId: CONFIG_URL.CLIENT_ID,
+            requiredUrlParams: ['scope'],
+            scope: ['likes'],
+            scopeDelimiter: '+',
+            authorizationEndpoint: CONFIG_URL.IG_AUTHORIZATION_ENDPOINT
+        });
     });
--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,3 +1,24 @@
 /**
  * Created by LuisGuillermo on 5/6/2015.
  */
+angular.module('Instagram')
+    .controller('HomeCtrl', function($scope, $window, $rootScope, $auth) {
+        /**
+         * Verifies if is authenticated.
+         */
+        $scope.isAuthenticated = function() {
+            return $auth.isAuthenticated();
+        };
+
+        /**
+         * Links the instagram account
+         */
+        $scope.linkInstagram = function() {
+            $auth.link('instagram')
+                .then(function(response) {
+                    $window.localStorage.currentUser = JSON.stringify(response.data.user);
+                    $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+                });
+        };
+
+    });
